1e7dd0830023b018257050398a0a768325e916e6
Андрей <40750040+AndrewStartcev@users.noreply.github.com>
2026-09-20 22:59:10 +0800

    fix: repair about page PHP syntax

--- page-about.php
+++ page-about.php
@@ -10,7 +10,9 @@ while ( have_posts() ) :
 
 	$hero_eyebrow = roverland_field( 'about_hero_eyebrow', '' );
 	$hero_title   = roverland_field( 'about_hero_title', get_the_title() );
-	$hero_lead    = roverland_field( 'about_hero_lead', '' );\n\t$hero_image   = roverland_field( 'about_hero_image', array() );\n\t$hero_image_url = roverland_image_url( $hero_image, 'assets/images/content/about-hero.png' );
+	$hero_lead      = roverland_field( 'about_hero_lead', '' );
+	$hero_image     = roverland_field( 'about_hero_image', array() );
+	$hero_image_url = roverland_image_url( $hero_image, 'assets/images/content/about-hero.png' );
 
 	$service_title   = roverland_field( 'about_service_title', '' );
 	$service_content = roverland_field( 'about_service_content', '' );
