1a67ef1dfc43d86e3d1eecda8fb35338f2ea4365
Андрей <40750040+AndrewStartcev@users.noreply.github.com>
2026-09-20 23:35:04 +0800

    feat: add promotions and portfolio post types

--- functions/post-types.php
+++ functions/post-types.php
@@ -27,5 +27,51 @@ add_action(
 				),
 			)
 		);
+
+		register_post_type(
+			'promotion',
+			array(
+				'labels' => array(
+					'name' => 'Акции',
+					'singular_name' => 'Акция',
+					'add_new_item' => 'Добавить акцию',
+					'edit_item' => 'Редактировать акцию',
+					'menu_name' => 'Акции',
+				),
+				'public' => true,
+				'has_archive' => false,
+				'show_in_rest' => false,
+				'menu_icon' => 'dashicons-tickets-alt',
+				'menu_position' => 22,
+				'supports' => array( 'title', 'page-attributes' ),
+				'rewrite' => array(
+					'slug' => 'aktsii',
+					'with_front' => false,
+				),
+			)
+		);
+
+		register_post_type(
+			'portfolio_item',
+			array(
+				'labels' => array(
+					'name' => 'Портфолио',
+					'singular_name' => 'Работа',
+					'add_new_item' => 'Добавить работу',
+					'edit_item' => 'Редактировать работу',
+					'menu_name' => 'Портфолио',
+				),
+				'public' => true,
+				'has_archive' => false,
+				'show_in_rest' => false,
+				'menu_icon' => 'dashicons-format-gallery',
+				'menu_position' => 23,
+				'supports' => array( 'title', 'page-attributes' ),
+				'rewrite' => array(
+					'slug' => 'portfolio',
+					'with_front' => false,
+				),
+			)
+		);
 	}
 );
